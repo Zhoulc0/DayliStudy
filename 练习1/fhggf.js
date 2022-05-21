519b5aef42d36e6861bf76b23a4167febd0f29d7
zhou <zhou1128009@gmail.com>
2022-05-21 14:07:45 +0800

    干得不错

---
+++ 练习1/fhggf.js
@@ -0,0 +1,14 @@
+
+// for(var i = 0; i < 100; i++){
+//    if(i % 3 == 0 || i % 5 ==0 || i % 8 ==0) {
+//        console.log(i+"")
+//    }
+//
+var n = parseInt(window.prompt('input'))
+var mul = 1;
+for(var i = 0; i < n; n++){ 
+     mul *= 2;
+} 
+     window.prompt(mul)
+
+
