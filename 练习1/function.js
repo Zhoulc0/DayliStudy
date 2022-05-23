e640af7cd21851fe42dbec9d7715614cb38165f0
zhou <zhou1128009@gmail.com>
2022-05-23 17:11:35 +0800

    第二天提交

--- 练习1/function.js
+++ 练习1/function.js
@@ -18,12 +18,62 @@
 
 // }
 // mul(5)
+// console.log(mul(5))
 
-function fd(n){
+// function fd(n){
    
-    if(n == 1 || n == 2 ){
-        return 1
-    }
-    return fd(n - 1) + fd(n - i)
+//     if(n == 1 || n == 2 ){
+//         return 1
+//     }
+//     return fd(n - 1) + fd(n - 2)
 
-}
+// }
+
+// console.log(fd(8));
+
+
+
+// function fn(a){
+//     console.log(a);
+
+//     var a =123;
+
+//     console.log(a);
+
+//     function a () {}
+
+//     console.log(a);
+
+//     var b = function () {}
+
+//     console.log(b);
+
+//     function d() {}
+
+
+// }
+// fn(1);
+
+// function bar() {
+//     return foo;
+//     foo = 10;
+//     function foo() {
+
+//     }
+//     var foo = 11
+// }
+// console.log(bar());
+
+//  console.log(bar());
+//  function bar() {
+//      foo=10;
+//      function foo() {
+
+//      }
+//      var foo = 11;
+//      return foo
+//  }
+
+
+
+
